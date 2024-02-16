acce546d5f3346f16935bf82313661c86bd43f0c
louisdvs <louis.vansenden@cultureamp.com>
2024-02-16 14:21:51 +0800

    Add pan to site button on the cards

--- src/DrawerCard.js
+++ src/DrawerCard.js
@@ -1,36 +1,52 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { Box } from '@mui/material';
+import * as React from "react"
+import Card from "@mui/material/Card"
+import CardActions from "@mui/material/CardActions"
+import CardContent from "@mui/material/CardContent"
+import CardMedia from "@mui/material/CardMedia"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import { Box } from "@mui/material"
+import { useMap } from "react-map-gl"
 
+export default function DrawerCard({ location }) {
+  const { denmarkMap } = useMap()
+  const onClick = () => {
+    denmarkMap.flyTo({
+      center: [location.geolocation.lng, location.geolocation.lat],
+    })
+  }
 
-export default function DrawerCard({location} )  {
-    console.log(location)
   return (
     <Box pt={2} pb={2}>
-        <Card sx={{ maxWidth: 345 }}>
-        <iframe width="320" height="180" src={location.youtube_url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <Card sx={{ maxWidth: 345 }}>
+        <iframe
+          width="320"
+          height="180"
+          src={location.youtube_url}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
         {/* <CardMedia
             sx={{ height: 140 }}
             image="{locations.Title}"
             title="location"
         /> */}
         <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-                {location.name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-                {location.description}
-            </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            {location.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {location.description}
+          </Typography>
         </CardContent>
-        {/* <CardActions>
-            <Button size="small">Learn More</Button>
-        </CardActions> */}
-        </Card>
+        <CardActions>
+          <Button size="small" onClick={onClick}>
+            Fly to site
+          </Button>
+        </CardActions>
+      </Card>
     </Box>
-  );
+  )
 }
