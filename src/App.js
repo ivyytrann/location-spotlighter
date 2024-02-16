7d091807e95cde7b6cff302b248bbaf8c135be26
Ivy <ivy.tran@cultureamp.com>
2024-02-16 15:53:11 +1100

    added videos to drawer cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import locations from './locations.json'
+// import locations from './locations.json'
+import videos from './videos.json'
 import DrawerCard from './DrawerCard';
 import { useRef } from 'react';
 import GeoMap from './Map';
@@ -23,12 +14,10 @@ const drawerWidth = 400;
 export default function LocationDrawer() {
 
   return (
-    
       <>
       <div style={{height:"100vh"}}>
         <GeoMap/>
       </div>
-      {/* </Box> */}
       <Drawer
         sx={{
           width: drawerWidth,
@@ -44,26 +33,11 @@ export default function LocationDrawer() {
 
         <Toolbar />
           <div align="center">
-            {locations.map((location, index) => (
+            {videos.map((location, index) => (
               <DrawerCard key={index} location={location} />
           ))}
           </div>
-        
-        {/* <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List> */}
-        
       </Drawer>
       </>
-    // </Box>
   );
 }
